df5dd2af4f2d02a68cd4b509d8d9ab4602e493ac
jeremyjflowers <rpgjuwangaming@gmail.com>
2021-04-01 11:39:03 -0500

    Game runs, but is unresponsive.

--- raygame/NodeGraph.cpp
+++ raygame/NodeGraph.cpp
@@ -3,12 +3,115 @@
 
 std::deque<NodeGraph::Node*> NodeGraph::findPath(Node* start, Node* end)
 {
-	// Find a path from start to end (The current implementation is obviously insufficient)
-	std::deque<Node*> path;
-	path.push_back(start);
-	path.push_back(start);
-	path.push_back(end);
-	return path;
+	std::deque<Node*> path; \
+		//Check if the start or the goal pointer is null
+		if (!start || !end)
+		{
+			//return an empty list
+			return path;
+		}
+
+	//Create a node pointer that will be act as an iterator for the graph
+	Node* currentNode = start;
+
+	//Create an open list
+	std::deque<Node*> openList;
+
+	//Create a closed list
+	std::deque<Node*> closedList;
+
+	//Add start to the open list
+	openList.push_front(start);
+
+	//Loop while the open list is not empty
+	while (!openList.empty())
+	{
+		//Sort the items in the open list by the f score
+		for (int i = 0; i < openList.size(); i++)
+		{
+			for (int j = openList.size(); j > i; j--)
+			{
+				if (openList[i]->fScore > openList[j - 1]->fScore)
+				{
+					Node* temp = openList[i];
+					openList[i] = openList[j - 1];
+					openList[j - 1] = temp;
+				}
+			}
+		}
+
+		//Set the iterator to be the first item in the open list
+		currentNode = openList.front();
+
+		//Check if the iterator is pointing to the goal node
+		if (currentNode == end)
+		{
+			//Return the new path found
+			return path;
+		}
+
+		//Pop the first item off the open list
+		openList.pop_front();
+
+		//Add the first item to the closed list
+		closedList.push_back(currentNode);
+
+		//Loop through all of the edges for the iterator
+		for (int i = 0; i < currentNode->connections.size(); i++)
+		{
+			//Create a node pointer to store the other end of the edge
+			Node* currentEdgeEnd = currentNode->connections[i].target;
+
+			//Check if node at the end of the edge is in the closed list
+			if (!isInList(closedList, currentEdgeEnd))
+			{
+
+				//Create a float and set it to be the g score of the iterator plus the cost of the edge
+				int tempGScore = currentNode->gScore + currentNode->connections[i].cost;
+
+				//Create a float and set it to be the h score of the node at the end of the edge
+				float tempHScore = currentEdgeEnd->hScore;
+
+				//Create a float for the f score and set it to be the g score combined with the h score
+				float tempFScore = tempGScore + tempHScore;
+
+				//Check if the node at the end of the edge is in the open list
+				if (!isInList(closedList, currentEdgeEnd))
+				{
+					//Set the nodes g score to be the g score calculated earlier
+					currentEdgeEnd->gScore = tempGScore;
+
+					//Set the nodes h score to be the h score calculated earlier
+					currentEdgeEnd->hScore = tempGScore;
+
+					//Set the nodes f score to be the f score calculated earlier
+					currentEdgeEnd->fScore = tempFScore;
+
+					//Set the nodes previous to be the iterator
+					currentEdgeEnd->previous = currentNode;
+
+					//Add the node to the open list
+					openList.push_back(currentEdgeEnd);
+				}
+
+				//Otherwise if the f score is less than the node at the end of the edge's f score...
+				else if (currentNode->fScore < currentEdgeEnd->fScore)
+				{
+					//Set its g score to be the g score calculated earlier
+					currentEdgeEnd->gScore = tempGScore;
+
+					//Set the nodes h score to be the h score calculated earlier
+					currentEdgeEnd->hScore = tempHScore;
+
+					//Set its f score to be the f score calculated earlier
+					currentEdgeEnd->fScore = tempFScore;
+
+					//Set its previous to be the current node
+					currentEdgeEnd->previous = currentNode;
+				}
+			}
+		}
+	}
 }
 
 void NodeGraph::drawGraph(Node* start)
@@ -50,4 +153,15 @@ void NodeGraph::drawConnectedNodes(Node* node, std::deque<Node*>* drawnList)
 			drawConnectedNodes(e.target, drawnList);
 		}
 	}
+}
+
+bool NodeGraph::isInList(std::deque<Node*> list, Node* node)
+{
+
+	for (int i = 0; i < list.size(); i++)
+	{
+		if (list[i] == node)
+			return true;
+	}
+	return false;
 }
